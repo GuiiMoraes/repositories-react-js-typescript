042db9c3d80b224a24e981b3c8ee25af403284a1
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-09 22:56:09 -0300

    style: create css of dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+import { Title, Form, Repositories } from './styles';
 
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="github explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <li>
+          <a href="to-do">
+            <img
+              src="https://avatars1.githubusercontent.com/u/18218319?s=460&u=f79fc531013cafd44e76b14ec393ebdbbeefe3a5&v=4"
+              alt="Guilherme Moraers"
+            />
+            <div>
+              <strong>GoBarber-Backend</strong>
+              <p>{}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        </li>
+        <li>
+          <a href="to-do">
+            <img
+              src="https://avatars1.githubusercontent.com/u/18218319?s=460&u=f79fc531013cafd44e76b14ec393ebdbbeefe3a5&v=4"
+              alt="Guilherme Moraers"
+            />
+            <div>
+              <strong>GoBarber-Backend</strong>
+              <p>{}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        </li>
+        <li>
+          <a href="to-do">
+            <img
+              src="https://avatars1.githubusercontent.com/u/18218319?s=460&u=f79fc531013cafd44e76b14ec393ebdbbeefe3a5&v=4"
+              alt="Guilherme Moraers"
+            />
+            <div>
+              <strong>GoBarber-Backend</strong>
+              <p>{}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        </li>
+      </Repositories>
+    </>
+  );
+};
 export default Dashboard;
